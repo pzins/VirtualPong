776e6e691046c280467b922d3ca4dc5a31c5e08e
zinspierre <zins.pierre@gmail.com>
2016-03-27 23:24:45 +0200

    peerlistlistener refait

--- app/src/main/java/com/example/pierre/myapplication/MyPeerListListener.java
+++ app/src/main/java/com/example/pierre/myapplication/MyPeerListListener.java
@@ -1,10 +1,12 @@
 package com.example.pierre.myapplication;
 
+import android.app.ProgressDialog;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,25 +17,27 @@ import java.util.List;
 public class MyPeerListListener implements WifiP2pManager.PeerListListener {
 
     WifiP2pDevice mDevice;
-    private List peers = new ArrayList();
+    private List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
+    private WifiP2pDevice device;
 
 
 
     public WifiP2pDevice getWifiP2pDevice()
+
     {
         return mDevice;
     }
 
     @Override
-    public void onPeersAvailable(WifiP2pDeviceList peers) {
-        // print all peers
-        //mDevice = peers.getDeviceList().iterator().next();
-        this.peers.clear();
-        this.peers.addAll(peers.getDeviceList());
-
-        for (WifiP2pDevice device : peers.getDeviceList()) {
-            Log.w("---------", device.deviceName);
+    public void onPeersAvailable(WifiP2pDeviceList peersList) {
+        peers.clear();
+        peers.addAll(peersList.getDeviceList());
+        if(peers.size() == 0)
+        {
+            Log.d(MainActivity.TAG, "No devices found");
+            return;
         }
+
     }
 
 }
