04a2b833966b815e8cfae17d690c1122697a4557
zinspierre <zins.pierre@gmail.com>
2016-04-01 22:23:04 +0200

    soket nettoayge debut

--- app/src/main/java/com/example/pierre/myapplication/DeviceDetailFragment.java
+++ app/src/main/java/com/example/pierre/myapplication/DeviceDetailFragment.java
@@ -97,52 +97,11 @@ public class DeviceDetailFragment extends Fragment implements WifiP2pManager.Con
                             e.printStackTrace();
                         }
                         new ClientAsyncTask(getActivity()).execute();
-
-
-
-    /*                        Socket socket = null;
-                            OutputStreamWriter osw;
-                            String str = "Hello World";
-                            try {
-                                socket = new Socket("192.168.49.1", 8988);
-                                osw =new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
-                                osw.write(str, 0, str.length());
-                            } catch (IOException e) {
-                                System.err.print(e);
-                            } finally {
-                                try {
-                                    socket.close();
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            }*/
-
-
-/*                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                        intent.setType("image");
-                        startActivityForResult(intent, CHOOSE_FILE_RESULT_CODE);*/
                     }
                 });
         return mContentView;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        // User has picked an image. Transfer it to group owner i.e peer using
-        // FileTransferService.
-        Uri uri = data.getData();
-        TextView statusText = (TextView) mContentView.findViewById(R.id.status_text);
-        statusText.setText("Sending: " + uri);
-        Log.d(MainActivity.TAG, "Intent----------- " + uri);
-        Intent serviceIntent = new Intent(getActivity(), FileTransferService.class);
-        serviceIntent.setAction(FileTransferService.ACTION_SEND_FILE);
-        serviceIntent.putExtra(FileTransferService.EXTRAS_FILE_PATH, uri.toString());
-        serviceIntent.putExtra(FileTransferService.EXTRAS_GROUP_OWNER_ADDRESS,
-                info.groupOwnerAddress.getHostAddress());
-        serviceIntent.putExtra(FileTransferService.EXTRAS_GROUP_OWNER_PORT, 8988);
-        getActivity().startService(serviceIntent);
-    }
 
 
     @Override
@@ -246,101 +205,24 @@ public class DeviceDetailFragment extends Fragment implements WifiP2pManager.Con
 
                 // Un PrintWriter possède toutes les opérations print classiques.
                 // En mode auto-flush, le tampon est vidé (flush) à l'appel de println.
-                PrintWriter pred = new PrintWriter(
-                        new BufferedWriter(
-                                new OutputStreamWriter(soc.getOutputStream())),
-                        true);
+                PrintWriter pred = new PrintWriter( new BufferedWriter(
+                                                    new OutputStreamWriter(soc.getOutputStream())),
+                                                    true);
 
                 while (true) {
                     String str = plec.readLine();          // lecture du message
                     if (str.equals("END")) break;
-                    System.out.println("ECHO = " + str);   // trace locale
-                    pred.println(str);                     // renvoi d'un écho
+                    System.out.println(str);   // trace locale
+                    pred.println("message recu");                     // renvoi d'un écho
                 }
                 plec.close();
                 pred.close();
                 soc.close();
-              /*  ServerSocket serverSocket = new ServerSocket(8988);
-                Log.w(MainActivity.TAG, "Server: Socket opened");
-                Socket client = serverSocket.accept();
-                Log.w(MainActivity.TAG, "Server: connection done");
-                Socket s = new Socket(InetAddress.getByName("stackoverflow.com"), 80);
-                PrintWriter pw = new PrintWriter(s.getOutputStream());
-                pw.print("GET / HTTP/1.1");
-                pw.print("Host: stackoverflow.com");
-                pw.print("");
-                pw.flush();
-                BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                String t;
-                while((t = br.readLine()) != null) System.out.println(t);
-                br.close();*/
-        /*        OutputStream mmOutStream = client.getOutputStream();
-                String str = "FEKIR";
-                mmOutStream.write(str.getBytes());*/
-
-
-                /*BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-                String line;
-
-                Log.w("--------------","############");
-                line = " " + reader.read();
-                Log.w("+++++", line);
-                while ((line = reader.readLine()) != null)
-                {
-                    sb.append(line).append("\n");
-                    Log.w("------", line);
-                }*/
-                //Log.w("RESULT! ! ! ! ", sb.toString());
-
-
-/*                final File f = new File(Environment.getExternalStorageDirectory() + "/"
-                        + context.getPackageName() + "/wifip2pshared-" + System.currentTimeMillis()
-                        + ".jpg");
-
-                File dirs = new File(f.getParent());
-                if (!dirs.exists())
-                    dirs.mkdirs();
-                f.createNewFile();
-
-                Log.d(MainActivity.TAG, "server: copying files " + f.toString());
-                InputStream inputstream = client.getInputStream();
-//                copyFile(inputstream, new FileOutputStream(f));
-                serverSocket.close();
-                return f.getAbsolutePath();
-            } catch (IOException e) {
-                Log.e(MainActivity.TAG, e.getMessage());
-                return null;
-            }*/
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
             return "OL";
-        }
-        /*
-         * (non-Javadoc)
-         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-         */
-        @Override
-        protected void onPostExecute(String result) {
-            if (result != null && false) {
-                statusText.setText("File copied - " + result);
-                Intent intent = new Intent();
-                intent.setAction(android.content.Intent.ACTION_VIEW);
-                intent.setDataAndType(Uri.parse("file://" + result), "image/*");
-                context.startActivity(intent);
-            }
-
-        }
-
-        /*
-         * (non-Javadoc)
-         * @see android.os.AsyncTask#onPreExecute()
-         */
-        @Override
-        protected void onPreExecute() {
-            statusText.setText("Opening a server socket");
         }
     }
 
@@ -369,39 +251,32 @@ public class DeviceDetailFragment extends Fragment implements WifiP2pManager.Con
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.println("SOCKET = " + socket);
 
             BufferedReader plec = null;
             try {
-                plec = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream())
-                );
+                plec = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
             PrintWriter pred = null;
             try {
-                pred = new PrintWriter(
-                        new BufferedWriter(
-                                new OutputStreamWriter(socket.getOutputStream())),
-                        true);
+                pred = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            pred.println("Lacazette Fekir Cornet Tolisso Umtiti Darder Ferri Kalulu");
             String str = "bonjour";
-           /* for (int i = 0; i < 10; i++) {
-                pred.println(str);          // envoi d'un message
+            for (int i = 0; i < 10; i++) {
+                pred.println("Lacazette Fekir Cornet Ghezzal Tolisso Umtiti Darder Ferri Kalulu");
                 try {
                     str = plec.readLine();      // lecture de l'écho
+                    System.out.println(str);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }*/
-            System.out.println("END");     // message de terminaison
-            pred.println("END") ;
+            }
+            pred.println("END");
             try {
                 plec.close();
             } catch (IOException e) {
@@ -413,65 +288,7 @@ public class DeviceDetailFragment extends Fragment implements WifiP2pManager.Con
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-           /* Socket socket = new Socket();
-            String str = "Lacazette\n";
-            try {
-                socket.connect((new InetSocketAddress("192.168.49.1", 8988)), 5000);
-                out = new PrintWriter(socket.getOutputStream());
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                String message = "";
-                int charsRead = 0;
-                char[] buffer = new char[2048];
-
-                while ((charsRead = in.read(buffer)) != -1) {
-                    message += new String(buffer).substring(0, charsRead);
-                }
-                Log.w("{{{{{{{{'  ", message);
-            } catch (IOException e) {
-                return "Error receiving response:  " + e.getMessage();
-            }*/
             return "OL";
         }
-        /*
-         * (non-Javadoc)
-         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-         */
-        @Override
-        protected void onPostExecute(String result) {
-            }
-
-        /*
-         * (non-Javadoc)
-         * @see android.os.AsyncTask#onPreExecute()
-         */
-        @Override
-        protected void onPreExecute() {
-        }
     }
-
-
-
-    public static boolean copyFile(InputStream inputStream, OutputStream out) {
-        byte buf[] = new byte[1024];
-        int len;
-        try {
-            while ((len = inputStream.read(buf)) != -1) {
-                out.write(buf, 0, len);
-
-            }
-            out.close();
-            inputStream.close();
-        } catch (IOException e) {
-            Log.d(MainActivity.TAG, e.toString());
-            return false;
-        }
-        return true;
-    }
-
 }
