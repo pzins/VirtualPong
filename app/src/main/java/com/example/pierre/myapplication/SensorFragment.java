99ef269b42116f27698ff53055315765ce9e3f94
FGuyet <florian.guyet@etu.utc.fr>
2016-05-14 02:59:03 +0200

    Bouncing ball integration
    Activity avec une balle qui rebondie contre les bords du smartphone

--- app/src/main/java/com/example/pierre/myapplication/SensorFragment.java
+++ app/src/main/java/com/example/pierre/myapplication/SensorFragment.java
@@ -1,13 +1,12 @@
 package com.example.pierre.myapplication;
 
 import android.app.Fragment;
-import android.app.ProgressDialog;
-import android.net.wifi.WpsInfo;
-import android.net.wifi.p2p.WifiP2pConfig;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.R;
 
 /**
  * Created by pierre on 03/04/16.
