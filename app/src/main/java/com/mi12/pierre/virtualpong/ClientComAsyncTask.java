63620c26c103ef97c11fef7e2eedec24c2d0e706
zinspierre <zins.pierre@gmail.com>
2016-05-25 10:28:28 +0200

    separation 2 fichiers ClientComAsyncTask

--- app/src/main/java/com/mi12/pierre/virtualpong/ClientComAsyncTask.java
+++ app/src/main/java/com/mi12/pierre/virtualpong/ClientComAsyncTask.java
@@ -3,7 +3,6 @@ package com.mi12.pierre.virtualpong;
 import android.content.Context;
 import android.os.AsyncTask;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
@@ -59,49 +58,4 @@ public class ClientComAsyncTask extends AsyncTask<Void, GamePositions, String> {
             this.gameView.setPositions(progress[0]);
         }
     }
-}
-
-
-//pour l'envoi des données (positions du jeu)
-class SendClientTask extends Thread
-{
-    private byte dir = 0x0;
-    private boolean shouldSend = false;
-    private String goIp;
-
-    public SendClientTask(String _ip){goIp = _ip;}
-
-    public void setDirection(byte _d){
-        dir = _d;
-        shouldSend = true;
-    }
-
-    public void run() {
-        Socket socket = null;
-        try {
-           socket = new Socket(goIp, 8988);
-        } catch (IOException e) {
-            e.printStackTrace();
-            run();
-        }
-        DataOutputStream dos = null;
-
-        try {
-            dos = new DataOutputStream(socket.getOutputStream());
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        synchronized (this){
-            while (true) {
-                try {
-                    dos.writeByte(dir);
-                    wait();
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-    }
-
 }
