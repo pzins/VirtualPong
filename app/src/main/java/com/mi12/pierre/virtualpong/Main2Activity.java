aed236178dea24638868bbf5bb89634214a04bd1
FGuyet <florian.guyet@etu.utc.fr>
2016-05-29 18:09:51 +0200

    Main Activity isn't a full screen

--- app/src/main/java/com/mi12/pierre/virtualpong/Main2Activity.java
+++ app/src/main/java/com/mi12/pierre/virtualpong/Main2Activity.java
@@ -14,55 +14,12 @@ import com.mi12.pierre.virtualpong.three_phones.reseau_local.ReseauLocal3Activit
 import com.mi12.pierre.virtualpong.three_phones.wifidirect.WifiDirect3Activity;
 import com.mi12.pierre.virtualpong.two_phones.wifidirect.WifiDirect2Activity;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 public class Main2Activity extends AppCompatActivity {
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private boolean mVisible;
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        hide_UI();
         setContentView(R.layout.activity_main2);
-        mContentView = findViewById(R.id.fullscreen_content);
 
         //Réseau local
         Button button_local_connect = (Button) findViewById(R.id.button_local_connect);
@@ -95,18 +52,6 @@ public class Main2Activity extends AppCompatActivity {
         });
     }
 
-    private void hide_UI() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
 }
 
 
