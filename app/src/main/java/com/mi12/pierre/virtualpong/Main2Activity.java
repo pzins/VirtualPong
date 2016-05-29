eba28148b4542e73bb159debb4f86d6ad7bdb474
FGuyet <florian.guyet@etu.utc.fr>
2016-05-29 19:32:10 +0200

    MainActivity redone

--- app/src/main/java/com/mi12/pierre/virtualpong/Main2Activity.java
+++ app/src/main/java/com/mi12/pierre/virtualpong/Main2Activity.java
@@ -1,10 +1,7 @@
 package com.mi12.pierre.virtualpong;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +9,7 @@ import android.widget.Button;
 import com.mi12.R;
 import com.mi12.pierre.virtualpong.three_phones.reseau_local.ReseauLocal3Activity;
 import com.mi12.pierre.virtualpong.three_phones.wifidirect.WifiDirect3Activity;
+import com.mi12.pierre.virtualpong.two_phones.reseau_local.ReseauLocal2Activity;
 import com.mi12.pierre.virtualpong.two_phones.wifidirect.WifiDirect2Activity;
 
 public class Main2Activity extends AppCompatActivity {
@@ -21,9 +19,19 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        //Réseau local
-        Button button_local_connect = (Button) findViewById(R.id.button_local_connect);
-        button_local_connect.setOnClickListener(new View.OnClickListener() {
+        //Réseau local 2 Smartphones
+        Button button_local_connect2 = (Button) findViewById(R.id.button_local_connect2);
+        button_local_connect2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, ReseauLocal2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Réseau local 2 Smartphones
+        Button button_local_connect3 = (Button) findViewById(R.id.button_local_connect3);
+        button_local_connect3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Main2Activity.this, ReseauLocal3Activity.class);
