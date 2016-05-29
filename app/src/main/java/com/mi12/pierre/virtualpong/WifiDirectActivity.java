e6639efb90ed884c51cff37bb88e11dc82df4504
zinspierre <zins.pierre@gmail.com>
2016-05-29 16:18:07 +0200

    nettoyage

--- app/src/main/java/com/mi12/pierre/virtualpong/WifiDirectActivity.java
+++ app/src/main/java/com/mi12/pierre/virtualpong/WifiDirectActivity.java
@@ -1,13 +1,20 @@
 package com.mi12.pierre.virtualpong;
 
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by pierre on 26/05/16.
  */
-
-public abstract class WifiDirectActivity extends AppCompatActivity implements WifiP2pManager.PeerListListener, WifiP2pManager.ConnectionInfoListener, DeviceListFragment.DeviceActionListener
+interface DeviceActionListener{
+    void showDetails(WifiP2pDevice device);
+    void cancelDisconnect();
+    void connect(WifiP2pConfig config);
+    void disconnect();
+}
+public abstract class WifiDirectActivity extends AppCompatActivity implements WifiP2pManager.PeerListListener, WifiP2pManager.ConnectionInfoListener, DeviceActionListener
 {
 
     public abstract void setIsWifiP2pEnabled(boolean state);
